77d2daf95d99f45e799c79cdcac75c370e397105
Michael <shamanoff@gmail.com>
2016-12-17 21:04:00 +0200

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,40 @@ import {HttpService} from "./http.service";
 })
 export class AppComponent implements OnInit {
 
+  items:any[] =[];
+  asyncString: any = this.httpService.getData();
   constructor(private httpService: HttpService) {
   }
 
   ngOnInit() {
-     this.httpService.getData()
+    this.httpService.getData()
       .subscribe(
-        (data: Response) => console.log(data)
+        (data: any) => console.log(data),
+        error =>console.log(error)
       );
-
   }
+
+  onSubmit(username: string, email: string) {
+    this.httpService.sendData({
+      username: username,
+      email: email
+    })
+      .subscribe(
+        data=>console.log(data)
+      );
+  }
+
+  onGetData(){
+    this.httpService.getOwnData()
+      .subscribe(
+        data=>{
+          const myArray = [];
+          for(let key in data){
+            myArray.push(data[key]);
+          }
+          this.items = myArray;
+        }
+      );
+  }
+
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Response, Headers} from "@angular/http";
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Injectable()
 export class HttpService {
@@ -8,7 +9,29 @@ export class HttpService {
   constructor(private http: Http) {
   }
 
-  getData(): Observable<any> {
-    return this.http.get('https://zenzen-f6f7c.firebaseio.com/title.json');
+  getData() {
+    return this.http.get('https://zenzen-f6f7c.firebaseio.com/title.json')
+      .map((response: Response) => response.json());
+  }
+
+  sendData(user: any) {
+    const body = JSON.stringify(user);
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('https://zenzen-f6f7c.firebaseio.com/data.json', body, {
+      headers: headers
+    })
+      .map((data: Response) => data.json())
+      .catch(this.handleError);
+  }
+
+  getOwnData(){
+    return this.http.get('https://zenzen-f6f7c.firebaseio.com/data.json')
+      .map((response: Response) => response.json());
+  }
+
+  private handleError(error: any){
+    console.log(error);
+    return Observable.throw(error.json());
   }
 }
